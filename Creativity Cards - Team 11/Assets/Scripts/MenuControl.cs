38e6c217756f52b735dfb201530c35e4aba95c58
Billy Burton <wjb.4tc@gmail.com>
2019-11-03 03:47:30 +0000

    Added a Unity Project to the repo. Within the project I have added a main menu, that is functional but in need of art and aesthetic work. There is also an empty scene for the main level.

---
+++ Creativity Cards - Team 11/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    public GameObject pnlCredits;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit Successful");
+        //Application.Quit();
+    }
+    
+    public void ShowCredits()
+    {
+        pnlCredits.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        pnlCredits.SetActive(false);
+    }
+}
